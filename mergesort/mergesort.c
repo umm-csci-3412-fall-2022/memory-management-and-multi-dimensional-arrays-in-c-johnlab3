e1dea1c2ebf8a364921de7dc82fa03f3fbde049c
jpwalbran <walbr042@morris.umn.edu>
2022-10-04 16:03:22 -0500

    Start implementation of mergesort
    Working on a beginning implementation of mergesort in mergesort.c,
    however, there are still a couple of failing tests, but we're committing for
    completeness' sake.
    
    Co-authored-by: BananaObserved <golds349@morris.umn.edu>

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,4 +1,9 @@
 #include "mergesort.h"
+#include <cstdlib>
+
+int needsSorting(int rangeSize);
+void mergeSortRange(int* input, int startIndex, int endIndex);
+void mergeRanges(int* input, int startIndex, int midIndex, int endIndex);
 
 void mergesort(int size, int values[]) {
   // This obviously doesn't actually do any *sorting*, so there's
@@ -8,5 +13,46 @@ void mergesort(int size, int values[]) {
   // `malloc/calloc` and `free`, so make sure you explicitly
   // allocate any new arrays that you need, even if you
   // might not strictly need to.
+  mergeSortRange(values, 0, size);
   return;
+}
+
+void mergeSortRange(int* input, int startIndex, int endIndex) {
+  if (!needsSorting(endIndex - startIndex)) {
+    return;
+  }
+  int midIndex = (endIndex + startIndex) / 2;
+  mergeSortRange(input, startIndex, midIndex);
+  mergeSortRange(input, midIndex, endIndex);
+  mergeRanges(input, startIndex, midIndex, endIndex);
+  return;
+}
+
+void mergeRanges(int* input, int startIndex, int midIndex, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  int* sorted = (int*) malloc(rangeSize * sizeof(int));
+  int firstIndex = startIndex;
+  int secondIndex = midIndex;
+  int copyIndex = 0;
+
+  while (copyIndex < rangeSize) {
+    if (firstIndex < midIndex && (secondIndex > endIndex || input[firstIndex] < input[secondIndex])) {
+      sorted[copyIndex] = input[firstIndex];
+      ++firstIndex;
+    }
+    else {
+     sorted[copyIndex] = input[secondIndex];
+     ++secondIndex;
+    }
+    ++copyIndex;
+  }
+  for (int i = 0; i < rangeSize; i++) {
+    input[startIndex + i] = sorted[i];
+  }
+  free(sorted);
+  return;
+}
+
+int needsSorting(int rangeSize) {
+  return rangeSize >= 2;
 }
