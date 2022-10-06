858b8ed5250f19d9c8adcab071ae42040d4ff474
jpwalbran <walbr042@morris.umn.edu>
2022-10-06 16:07:43 -0500

    Add first array_merge implementation
    Wrote a first attempt to create the array merge, currently the tests fail with a segmentation fault.
    
    Co-authored-by: BananaObserved <golds349@morris.umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,7 +1,51 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
+#include <stdlib.h>
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+  //Figure out the total number of elements for the new array
+  //This starts at 1 to account for the first element, being the array size
+  //Note
+  int total_size = 1;
+  for (int i = 0; i < num_arrays; ++i){
+    total_size += sizes[i];
+  }
+  int *output = (int*) malloc(total_size + 1 * sizeof(int));
+  output[0] = total_size - 1; //The total number of actual elements in the array, not counting size
+
+  //Sort all of the subarrays
+  for(int i = 0; i < num_arrays; ++i){
+    mergesort(sizes[i], values[i]);
+  }
+
+  //Merge the now-sorted arrays, eliminating duplicates
+  //Store the position of travel within the newly-sorted arrays and where we're writing to
+  int* positions = (int*) calloc(num_arrays, sizeof(int));
+  int write_position = 1;
+
+  while(true){
+    //the array with the current smallest value; set to -1
+    //to detect if no changes are made.
+    int smallest_position = -1;
+
+    for(int i = 0; i < num_arrays; i++){
+      if(positions[i] < sizes[i]){
+        int current_value = values[i][positions[i]];
+        if(smallest_position == -1 || output[write_position] > current_value){
+          smallest_position = i;
+          output[write_position] = current_value;
+        } else if (output[write_position] == current_value){
+          ++positions[i]; //this is a duplicate, we don't want to work with this
+        }
+      }
+    }
+
+    if(smallest_position == -1) {break;}
+    ++positions[smallest_position];
+    ++write_position;
+  }
+
+  output = (int*) realloc(output, (write_position - 1) * sizeof(int));
+  free(positions);
+  return output;
 }
