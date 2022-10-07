2c2157820cd6321288df3d5105293310b71c8f65
jpwalbran <walbr042@morris.umn.edu>
2022-10-06 19:29:19 -0500

    Fix array_merge implementation
    Finished alternative implementation for the array_merge function,
    including checking against tests, but not the valgrind for leaks,
    due to a lack of main function not allowing local running of valgrind.
    Also removed unnecessary comments from mergesort.c
    
    Co-authored-by: BananaObserved <golds349@morris.umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,56 +1,14 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
 #include <stdlib.h>
-
-int* array_merge_old(int num_arrays, int* sizes, int** values) {
-  //Figure out the total number of elements for the new array
-  //This starts at 1 to account for the first element, being the array size
-  //Note
-  int total_size = 1;
-  for (int i = 0; i < num_arrays; ++i){
-    total_size += sizes[i];
-  }
-  int *output = (int*) malloc(total_size + 1 * sizeof(int));
-  output[0] = total_size - 1; //The total number of actual elements in the array, not counting size
-
-  //Sort all of the subarrays
-  for(int i = 0; i < num_arrays; ++i){
-    mergesort(sizes[i], values[i]);
-  }
-
-  //Merge the now-sorted arrays, eliminating duplicates
-  //Store the position of travel within the newly-sorted arrays and where we're writing to
-  int* positions = (int*) calloc(num_arrays, sizeof(int));
-  int write_position = 1;
-
-  while(true){
-    //the array with the current smallest value; set to -1
-    //to detect if no changes are made.
-    int smallest_position = -1;
-
-    for(int i = 0; i < num_arrays; i++){
-      if(positions[i] < sizes[i]){
-        int current_value = values[i][positions[i]];
-        if(smallest_position == -1 || output[write_position] > current_value){
-          smallest_position = i;
-          output[write_position] = current_value;
-        } else if (output[write_position] == current_value){
-          ++positions[i]; //this is a duplicate, we don't want to work with this
-        }
-      }
-    }
-
-    if(smallest_position == -1) {break;}
-    ++positions[smallest_position];
-    ++write_position;
-  }
-
-  output = (int*) realloc(output, (write_position - 1) * sizeof(int));
-  free(positions);
-  return output;
-}
+#include <stdio.h>
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
+  if (num_arrays == 0) {
+    int* output = (int*) malloc(sizeof(int));
+    output[0] = 0;
+    return output;
+  }
   //Find the total length of all of the arrays
   int total_size = 0;
 
@@ -66,11 +24,23 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
      insert_position++;
    }
   }
-  mergesort(merged_array, total_size);
-  int unique_count = !!total_size;
+  mergesort(total_size, merged_array);
+  int unique_count = 1;
 
   for (int i = 1; i < total_size; i++) {
     unique_count += (merged_array[i] != merged_array[i - 1]);
   }
 
+  int* output = (int*) malloc((unique_count + 1) * sizeof(int));
+  //reuse the insert position from above.
+  insert_position = 1;
+  for (int i = 0; i < total_size; i++) {
+   if (i == 0 || merged_array[i] != merged_array[i - 1]) {
+     output[insert_position] = merged_array[i];
+     ++insert_position;
+   }
+  }
+  output[0] = unique_count;
+  free(merged_array);
+  return output;
 }
--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -6,13 +6,6 @@ void mergeSortRange(int* input, int startIndex, int endIndex);
 void mergeRanges(int* input, int startIndex, int midIndex, int endIndex);
 
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
   mergeSortRange(values, 0, size);
   return;
 }
